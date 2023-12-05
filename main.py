13458607c162f971b2513050344819a44fee4dfa
Will Dingle <86018656+willdingle@users.noreply.github.com>
2023-12-05 12:45:36 +0000

    Started tf_idf (not working currently)

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import regex as re
 from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
-import json
 import pickle
+import math
 
 def init():
     global vocabIndex
@@ -13,6 +12,7 @@ def init():
     global docIDs
     global docIDsIndex
     global postings
+    global totalTerms
     if os.path.isfile("vocab.pkl"):
         file = open("vocab.pkl", "rb")
         vocab = pickle.load(file)
@@ -22,6 +22,9 @@ def init():
         file.close()
         file = open("postings.pkl", "rb")
         postings = pickle.load(file)
+        file.close()
+        file = open("totalTerms.pkl", "rb")
+        totalTerms = pickle.load(file)
         file.close()
     else:
         fileNumber = 1
@@ -33,6 +36,7 @@ def init():
             docIDs.update({docIDsIndex : fileName})
 
             fileTokens = processPage(fileContents)
+            totalTerms.update({docIDsIndex : len(fileTokens)})
             for token in fileTokens:
                 if token not in vocab:
                     vocab.update({token : vocabIndex})
@@ -52,21 +56,24 @@ def init():
             file = open("postings.pkl", "wb")
             pickle.dump(postings, file)
             file.close()
+            file = open("totalTerms.pkl", "wb")
+            pickle.dump(totalTerms, file)
+            file.close()
             print(f"{fileNumber}: {fileName} finished processing")
             fileNumber += 1
 
 # Process contents of html file and return the tokens
 def processPage(page):
-    stops = set(stopwords.words('english'))
+    stops = set(stopwords.words("english"))
     # Parse html using beautifulsoup
     soup = BeautifulSoup(page, "html.parser")
-    # Find all p elements
+    # Find all div elements
     paras = soup.find_all("div")
     cleanedTokens = {}
     # Go through each paragraph and remove punctuation, convert to lowercase and remove stopwords
     for p in paras:
         cleanedParas = p.get_text()
-        reText = re.sub(r"[^A-Za-z ]+", "", str(cleanedParas))
+        reText = re.sub(r"[^A-Za-z ]-+", "", str(cleanedParas))
         words = word_tokenize(reText)
         for word in words:
             word = word.lower()
@@ -85,6 +92,7 @@ def getDocIDs(term):
     return False
 
 def processQuery(query):
+    query = re.sub(r"[^A-Za-z ]-+", "", query)
     # Splits search query into terms and gets doc IDs of docs containing each term
     queryTerms = query.split(" ")
     docsFound = []
@@ -96,6 +104,22 @@ def processQuery(query):
         print("----- " + query + " -----")
         print("NO RESULTS")
         return
+    
+    # numDocsTotal = no of docs in total
+        # df = document frequency (no of docs term has occured in)
+        # term freq in curr doc * log(numDocsTotal / df) = score
+    for docsContaining in docsFound:
+        for docContaining in docsContaining:
+            tf = docsContaining[docContaining]
+            print(tf, end=":")
+            numDocsTotal = 399
+            df = len(docsContaining)
+            print(df, end=":")
+            idf = math.log(numDocsTotal / df)
+            print(idf, end=":")
+            tf_idf = tf * idf
+            print(tf_idf)
+            docsContaining[docContaining] = tf_idf
 
     # Finds and prints docs where all terms are found
     docsContainingAll = {}
@@ -123,13 +147,15 @@ def processQuery(query):
         resultsDisp += 1
         if resultsDisp == 10: break
 
-vocab = {}
+vocab = {} # {term : vocabID}
 vocabIndex = 0
-docIDs = {}
+docIDs = {} # {docID : docName}
 docIDsIndex = 0
 postings = {} # {vocabID : {docID : freq, docID: freq}}
+totalTerms = {} # {docID : numOfTerms}
 init()
 while True:
     query = input("Search (0 to quit): ")
     if query == "0": break
     processQuery(query)
+    print(totalTerms)
